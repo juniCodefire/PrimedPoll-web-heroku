229be8d96ae1686f759daba5c5bd9fe57962cb97
Mr-Juni <junipreach2017@gmail.com>
2019-07-23 12:39:18 +0100

    database issue

--- routes/web.php
+++ routes/web.php
@@ -165,6 +165,6 @@ $router->group(['middleware' => 'jwt.auth', 'prefix' => 'api'], function() use (
 $router->group(['middleware' => 'usernameCheck', 'prefix' => 'api'], function() use ($router)
 {
     //JuniCodefire
-    $router->get('profile/{permission}/{onSession}/{username}/', 'UserPublicProfile@showData');
+    $router->get('profile/{permission}/{onSession}/{username}', 'UserPublicProfile@showData');
     $router->get('public/feeds/{id}/{username}', 'UserFeedsController@usersFeeds');
 });
