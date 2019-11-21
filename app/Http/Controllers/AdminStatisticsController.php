9347412a132f2bca7e0918aa2ea58f716b1dae00
unknown <junicodefire@gmail.com>
2019-11-21 10:05:06 +0100

    rule

--- app/Http/Controllers/AdminStatisticsController.php
+++ app/Http/Controllers/AdminStatisticsController.php
@@ -79,8 +79,9 @@ class AdminStatisticsController extends Controller
             ->get();   
 
         foreach ($t_p_by_v as $t) {
-            $poll = Poll::where('id', $t->poll_id)->first();
-            array_push($this->trending_polls_by_votes , ['poll' => $poll, 'voteCount' =>$t->totalVote]);
+            $poll = Poll::where('id', $t->poll_id)->with('users')->first();
+            array_push($this->trending_polls_by_votes , [
+                'image_link' => 'https://res.cloudinary.com/getfiledata/image/upload/w_200,c_thumb,ar_4:4,g_face/',  'poll' => $poll, 'voteCount' =>$t->totalVote]);
         }
 
         return response()->json(
